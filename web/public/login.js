7296f6be90f7e8542c183f1962cc606c0781e2b3
Xu He <hexu16461426@gmail.com>
2018-04-14 19:01:48 -0400

    "login database"

--- web/public/login.js
+++ web/public/login.js
@@ -1,5 +1,13 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+  } else {
+    // No user is signed in.
+  }
+});
+
 function login(){
-	var userEmail = document.getElementById("userEmail").value;
+	var userEmail = document.getElementById("email_field").value;
 	var userPass = document.getElementById("password_field").value;
 
 	window.alert(userEmail + userPass);
