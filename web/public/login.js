6eaaed714a88ced104a279f9d8feba2e42ded186
Xu He <hexu16461426@gmail.com>
2018-04-17 15:20:55 -0400

    "update UI"

--- web/public/login.js
+++ web/public/login.js
@@ -1,21 +1,21 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-    document.getElementById("user_div").style.display = "initial";
+    document.getElementById("user_div").style.display = "block";
     document.getElementById("login_div").style.display = "none";
 
     var user = firebase.auth().currentUser;
 
     if(user != null) {
     	var email_id = user.email;
-
-    	document.getElementById("user_p").innerHTML = email_id + ", ";
+    	var email_verified = user.emailVerified;
+    	document.getElementById("user_p").innerHTML = "Spotter: " + email_id;
     }
 
   } else {
     // No user is signed in.
     document.getElementById("user_div").style.display = "none";
-    document.getElementById("login_div").style.display = "initial";
+    document.getElementById("login_div").style.display = "block";
   }
 });
 
